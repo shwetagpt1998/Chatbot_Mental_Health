7beafddb3ca435e0d74c5796c2f56ac4dbc14c1c
ShwetaBhattacharjee <shwetaparna111@gmail.com>
2024-09-26 20:38:14 +0400

    Initial commit

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def predict():
     return jsonify({'emotion': prediction, 'response': response, 'emoji': emoji})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
